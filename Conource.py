d0c692e17a71c1e045713c2d343870b700c3b650
blankdvth <contact@blankdvth.com>
2023-04-22 13:44:30 -0400

    Squashed Commit

--- Conource.py
+++ Conource.py
@@ -5,20 +5,36 @@ from os.path import isfile, isdir, expanduser
 from telnetlib import Telnet
 from time import sleep
 from typing import Union
+from re import compile
 from shlex import split as ssplit
 from sys import platform as sysplatform
 
+VERSION = "0.0.1"
+ARG_REGEX = compile(r"\${{(\d+)}}")
+
+home = expanduser("~")
+platform_locations = {"linux": ".config", "darwin": ".config"}
+if "use_project_dir" in environ and environ["use_project_dir"] == "1":
+    DATA_PATH = "data"
+elif sysplatform.startswith("win"):
+    DATA_PATH = environ["APPDATA"] + "\\Conource"
+else:
+    DATA_PATH = home + "/" + platform_locations[sysplatform] + "/Conource"
+
+
+class ConsoleHandler(logging.Handler):
+    def emit(self, record):
+        message = (
+            record.getMessage()
+            .replace('"', "'")
+            .replace(";", "<SEMICOLON>")
+            .split("\n")[0]
+        )
+        if config["logging_console"] and record.levelno >= logging.WARNING:
+            tn.write(f'echo "[Conource] [{record.levelname}] {message}"\n'.encode())
+
 
 def load_config() -> dict:
-    home = expanduser("~")
-    platform_locations = {"linux": ".config", "darwin": ".config"}
-    if "use_project_dir" in environ and environ["use_project_dir"] == "1":
-        DATA_PATH = "data"
-    elif sysplatform.startswith("win"):
-        DATA_PATH = environ["APPDATA"] + "\\Conource"
-    else:
-        DATA_PATH = home + "/" + platform_locations[sysplatform] + "/Conource"
-
     if not isdir(DATA_PATH):
         mkdir(DATA_PATH)
     if not isfile(DATA_PATH + "/config.json"):
@@ -27,10 +43,12 @@ def load_config() -> dict:
                 {
                     "host": "127.0.0.1",
                     "port": 16323,
-                    "check_delay": 5,
+                    "check_delay": 0,
+                    "fail_check_delay": 5,
                     "console_prefix": "cn_",
-                    "password": None,
+                    "block_semicolon": True,
                     "logging_level": "INFO",
+                    "logging_console": True,
                     "commands": {},
                 },
                 f,
@@ -38,6 +56,8 @@ def load_config() -> dict:
             )
     with open(DATA_PATH + "/config.json", "r") as f:
         return load(f)
+
+    # TODO: Implement automatic config updates
 
 
 def replace_args(command: str, args: list) -> str:
@@ -61,10 +81,10 @@ def read():
             except ConnectionRefusedError:
                 logging.debug(
                     "Connection refused, retrying in {} seconds".format(
-                        config["check_delay"]
+                        config["fail_check_delay"]
                     )
                 )
-                sleep(config["check_delay"])
+                sleep(config["fail_check_delay"])
         return ""
 
 
@@ -83,7 +103,28 @@ if __name__ == "__main__":
     logging.basicConfig(
         level=config["logging_level"],
         format="[%(asctime)s] [%(levelname)s] %(message)s",
-        handlers=[logging.StreamHandler(), logging.FileHandler("data/conource.log")],
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(DATA_PATH + "/conource.log"),
+            ConsoleHandler(),
+        ],
+    )
+    logging.addLevelName(
+        logging.WARNING + 1, "INFO - IMPORTANT"
+    )  # Will log to console as well
+    setattr(logging, "INFOIMP", logging.WARNING + 1)
+    setattr(
+        logging,
+        "infoimp",
+        lambda message, *args, **kwargs: logging.log(
+            logging.INFOIMP, message, *args, **kwargs
+        ),
+    )
+    logging.info("Conource v{}".format(VERSION))
+    logging.info(
+        "Loaded commands: {}".format(
+            ", ".join([config["console_prefix"] + i for i in config["commands"].keys()])
+        )
     )
     tn = None
     while tn is None:
@@ -93,27 +134,74 @@ if __name__ == "__main__":
         except ConnectionRefusedError:
             logging.debug(
                 "Connection refused, retrying in {} seconds".format(
-                    config["check_delay"]
+                    config["fail_check_delay"]
                 )
             )
-            sleep(config["check_delay"])
+            sleep(config["fail_check_delay"])
 
     while True:
-        line = read()
-        while line != "":
-            if line.startswith(config["console_prefix"]):
-                command_raw = line[len(config["console_prefix"]) :].strip()
-                command_name = command_raw.split(" ")[0]
-                if command_name in config["commands"]:
-                    command_args = ssplit(command_raw[len(command_name) :])
-                    commands = config["commands"][command_name]
-                    logging.info("Running command '{}'".format(command_raw))
-                    send(commands, command_args)
-                else:
-                    logging.warning(
-                        "Command {}{} not found".format(
-                            config["console_prefix"], command_name
-                        )
-                    )
+        try:
             line = read()
-        sleep(5)
+            while line != "":
+                if line.startswith(config["console_prefix"]):
+                    command_raw = line[len(config["console_prefix"]) :].strip()
+                    command_name = command_raw.split(" ")[0]
+                    if command_name == "exit":
+                        logging.infoimp("Exiting")
+                        exit(0)
+                    elif command_name == "help":
+                        avail_commands = {}
+                        for command, commands in config["commands"].items():
+                            avail_commands[command] = (
+                                int(max(ARG_REGEX.findall("".join(commands)), key=int))
+                                + 1
+                            )
+                        logging.infoimp(
+                            "Available commands: {}".format(
+                                ", ".join(
+                                    f"{i} ({j})" for i, j in avail_commands.items()
+                                )
+                            )
+                        )
+                    elif command_name == "reload":
+                        config = load_config()
+                        logging.infoimp("Reloaded config")
+                    elif config["block_semicolon"] and ";" in command_raw:
+                        logging.warning(
+                            "Did not execute '{}' because it contains a semicolon".format(
+                                command_raw
+                            )
+                        )
+                        line = read()
+                        continue
+                    elif command_name in config["commands"]:
+                        command_args = ssplit(command_raw[len(command_name) :])
+                        commands = config["commands"][command_name]
+                        max_args = (
+                            int(max(ARG_REGEX.findall("".join(commands)), key=int)) + 1
+                        )
+                        if len(command_args) != max_args:
+                            logging.error(
+                                "{} arguments for command '{}', got {}, expected {}".format(
+                                    "Too many"
+                                    if len(command_args) > max_args
+                                    else "Not enough",
+                                    command_name,
+                                    len(command_args),
+                                    max_args,
+                                )
+                            )
+                            line = read()
+                            continue
+                        logging.info("Running command '{}'".format(command_raw))
+                        send(commands, command_args)
+                    else:
+                        logging.warning(
+                            "Command {}{} not found".format(
+                                config["console_prefix"], command_name
+                            )
+                        )
+                line = read()
+        except (Exception,) as e:
+            logging.exception(repr(e))
+        sleep(config["check_delay"])
--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # Conource
 
+Conource is a program that allows you to define your own custom commands (with arguments) for the CS:GO console.
+
+## Prerequisites
+
+- Python 3
+
+## Installation
+
+- Download the [latest release](https://github.com/blankdvth/Conource/releases/latest) (as a ZIP file)
+- Extract the ZIP file to where you want the program to be installed
+- Open a terminal in the extracted folder
+- Run the program once (so that data files are created)
+
+## Setup
+
+- Open your Steam library
+- Right click on CS:GO and click on `Properties`
+- In the Launch Options field, enter `-netconport 16323` (this is the default port the program uses, you can change it
+  in the config file)
+
+## Configuration
+
+The data folder is located in your config folder (on Windows, this
+is `C:\Users\YOUR_USERNAME\AppData\Roaming\Conource`).
+Open the configuration file (`config.json`). Below is an outline of what each option does, and how to configure your
+aliases.
+
+### Settings
+
+- **`host`**: The host to bind to. This is almost always `127.0.0.1` or `localhost`.
+- **`port`**: The port to bind to. This is the port you entered in the CS:GO launch options.
+- **`check_delay`**: The delay between each full readthrough of the console. This is in seconds.
+- **`fail_check_delay`**: How often the connection is retried when it fails. This is in seconds.
+- **`console_prefix`**: The prefix that is expected to be before each command.
+- **`block_semicolon`**: Whether to block semicolons in command arguments. This is HIGHLY RECOMMENDED, disabling it can
+  leave you vulnerable to command injection.
+- **`logging_level`**: The amount of logging shown. This can be one of `DEBUG`, `INFO`, `WARNING`, `ERROR`, `CRITICAL`.
+- **`logging_console`**: If enabled, logs of level `WARNING` and above will be shown in the console.
+- **`commands`**: Dictionary of commands. See next section for more information.
+
+### Commands
+
+Commands are the main feature of the program, and are created via the `commands` section of the config file. All
+commands are executed by an `echo` command in console, followed by your command prefix, command name, and arguments.
+
+Each command is in the format of:
+
+```json
+"command_name": [
+  "command1",
+  "command2",
+  "etc"
+]
+```
+
+The `command_name` is the name that is used to invoke the command, it is case-sensitive. Each command has an array of
+actual commands to run.
+Placeholders can be used in these commands to insert arguments. They are in the format `${{NUM}}`, where `NUM` is the
+position/index of the argument (starting from 0).
+Here's an example that will send a message in chat, greeting the first argument:
+
+```json
+"hello": [
+  "say Hello, ${{0}}!"
+]
+```
+
+If the `console_prefix` is `cn_`, then the command can be invoked with `echo cn_hello World` in console (output would
+be `Hello, World!`).
+
+Keep in mind the program needs to be restarted for changes to take effect.
+
+## Usage
+
+All commands can be invoked by opening the CS:GO console (default key is `~`, needs to be enabled in settings), and
+typing:
+`echo <command prefix><command name> <arguments>}`, for example if the command prefix were `cn_`, the command
+were `hello`
+, and it took one argument: `echo cn_hello John`.
+
+### Default Commands
+There are a few default commands that perform various actions, they are checked before your commands so you cannot have
+duplicate names. All default commands must be prefixed with the command prefix.
+
+- **exit**: This exits the program.
+- **help**: This shows a list of commands, and how many arguments they take.
+- **reload**: This reloads the config file (not all changes can be applied via reload, such as logging changes. A restart may be needed)
+
+## Common Issues
+
+### CS adds spaces to my arguments
+
+There are certain special characters that CS pads with spaces, `:` is one example. To get around this, you can add
+double-quotes around the entire command, for example: `echo "cn_hello Person:John"`. Alternatively, if you're afraid
+you'll forget, you can replace the one argument with 3 (one before, one colon, one after). `say Hello ${{0}}` becomes
+`say Hello ${{0}}${{1}}${{2}}`.
